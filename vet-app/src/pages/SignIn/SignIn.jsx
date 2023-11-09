c45e90b98605c48003cae04fbc9fea1ea1f62afa
julia-bit <80833377+julia-bit@users.noreply.github.com>
2023-11-09 20:18:57 +0100

    done first signIn page

--- vet-app/src/pages/SignIn/SignIn.jsx
+++ vet-app/src/pages/SignIn/SignIn.jsx
@@ -23,7 +23,7 @@ const SignIn = () => {
             required
           />
         </div>
-        
+
         <input className="email" type="email" placeholder="Email" required />
         <input
           className="password"
@@ -33,24 +33,47 @@ const SignIn = () => {
         />
         <input className="address" type="text" placeholder="Adres" required />
         <div className="cityCode">
-        <input
-          type="text"
-          name="postcode"
-          className="postcode"
-          pattern="\d{2}-\d{3}"
-          placeholder="Kod pocztowy"
-          required
-        />
-        <input className="city" type="text" placeholder="Miasto" required />
+          <input
+            type="text"
+            name="postcode"
+            className="postcode"
+            pattern="\d{2}-\d{3}"
+            placeholder="Kod pocztowy"
+            required
+          />
+          <input className="city" type="text" placeholder="Miasto" required />
         </div>
         <input className="phone" type="number" placeholder="Telefon" required />
-        <select name="Zawód" className="role">
-          <option value="vet">Weterynarz</option>
-          <option value="admin">Administrator</option>
-          <option value="reception">Recepcjonista</option>
-          <option value="patient">Patient</option>
-          <option value="me">Me</option>
-        </select>
+        <div>
+          <select name="Zawód" className="roles">
+            <option value="vet">Weterynarz</option>
+            <option value="admin">Administrator</option>
+            <option value="reception">Recepcjonista</option>
+            <option value="patient">Pacjent</option>
+            <option value="me">Me</option>
+          </select>
+          <select name="Klinika" className="clinics">
+            <option value="one">Klinika 1</option>
+            <option value="two">Klinika 2</option>
+            <option value="three">Klinika 3</option>
+            <option value="four">Klinika 4</option>
+            <option value="me">Me</option>
+          </select>
+        </div>
+        <span className="flex mt-6 flex-between items-center">
+          <span className=" flex justify-start whitespace-nowrap text-sm">
+            <h5>Masz już konto?</h5>
+            <a href="http://localhost:3000/login" className="ml-1 font-semibold">Zaloguj się.</a>
+          </span>
+          <footer className="flex justify-end w-96">
+            <button
+              type="submit"
+              className="bg-customGrey hover:bg-blue-600 px-10 py-2 rounded  text-black hover:shadow-md"
+            >
+              Zaloguj się
+            </button>
+          </footer>
+        </span>
       </div>
     </div>
   );
