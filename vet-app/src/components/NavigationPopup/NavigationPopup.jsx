ecdeedd152f6ce31132bbf9420510f7aa99cf00b
julia-bit <80833377+julia-bit@users.noreply.github.com>
2023-12-03 18:01:21 +0100

    add medications form

--- vet-app/src/components/NavigationPopup/NavigationPopup.jsx
+++ vet-app/src/components/NavigationPopup/NavigationPopup.jsx
@@ -1,26 +1,40 @@
-import React, { useState } from "react";
+import React from "react";
+import { useNavigate } from "react-router-dom";
 import "./NavigationPopup.css";
 
 const PopupComponent = () => {
+  let navigate = useNavigate();
+
+  const navigateToPatient = () => {
+    let path = "/add-patient";
+    navigate(path);
+  };
+
+  const navigateToIllness = () => {
+    let path = "/add-illness";
+    navigate(path);
+  };
+
+  const navigateToMedication = () => {
+    let path = "/add-medication";
+    navigate(path);
+  };
+
+  const navigateToVisitType = () => {
+    let path = "/add-visit-type";
+    navigate(path);
+  };
+
   return (
     <div className="navigationPopup-overlay">
       <div className="popup-content">
         <p className="navigation-message">Co zamierzasz dodać?</p>
-      
-      <div className="nav-buttons">
-        <button onClick={() => console.log("Navigate to Button 1")}>
-          Pacjenta
-        </button>
-        <button onClick={() => console.log("Navigate to Button 2")}>
-          Chorobę
-        </button>
-        <button onClick={() => console.log("Navigate to Button 3")}>
-          Lek
-        </button>
-        <button onClick={() => console.log("Navigate to Button 3")}>
-          Typ wizty
-        </button>
-      </div>
+        <div className="nav-buttons">
+          <button onClick={navigateToPatient}>Pacjenta</button>
+          <button onClick={navigateToIllness}>Chorobę</button>
+          <button onClick={navigateToMedication}>Lek</button>
+          <button onClick={navigateToVisitType}>Typ wizyty</button>
+        </div>
       </div>
     </div>
   );
