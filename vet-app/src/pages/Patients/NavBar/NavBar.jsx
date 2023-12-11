f54232e0697edc165618eca650707d041ed6c180
Jan Szczuka <janogamesyt5@gmail.com>
2023-12-11 17:26:06 +0100

    add action confirmation to illness history form

--- vet-app/src/pages/Patients/NavBar/NavBar.jsx
+++ vet-app/src/pages/Patients/NavBar/NavBar.jsx
@@ -1,38 +1,14 @@
 import React, { useState, useEffect } from "react";
 import "./NavBar.css";
-import VisitForm from "../../../pages/VisitForm/VisitForm";
-import { createVisitRequest } from "../../../api/visitsRequest";
 import { NavBarData } from "./NavBarData";
 import { Link } from "react-router-dom";
 import PrescriptionForm from "../../../PrescriptionForm/PrescriptionForm";
-import dayjs from "dayjs";
 import IllnessHistoryForm from "../../IllnessHistoryForm/IllnessHistoryForm";
 import { createIllnessHistoryRequest } from "../../../api/illnessHistoryRequests";
 
 
 const NavBar = ({ id, onSelectOption, selectedTab }) => {
-
-  const [showIllnessHistoryForm, setShowIllnessHistoryForm] = useState(false);
   const [showPrescriptionForm, setShowPrescriptionForm] = useState(false);
-
-  const handleIllnessHistoryForm = () => {
-    setShowIllnessHistoryForm(true);
-  };
-
-  const handleCloseIllnessHistoryForm = () => {
-    setShowIllnessHistoryForm(false);
-  };
-
-  const submitIllnessForm = async (formData) => {
-    try {
-      console.log("Form Data:", formData);
-
-      await createIllnessHistoryRequest(formData);
-    } catch (error) {
-      console.error("Error submitting form:", error);
-    }
-    handleCloseIllnessHistoryForm();
-  };
 
   const handleCreatePrescription = () => {
     setShowPrescriptionForm(true);
@@ -42,7 +18,6 @@ const NavBar = ({ id, onSelectOption, selectedTab }) => {
     setShowPrescriptionForm(false);
   };
 
-  const isChorobyTab = selectedTab === "HISTORIA CHORÓB";
   const isDokumentacjaTab = selectedTab === 'DOKUMENTACJA';
 
   return (
@@ -63,23 +38,6 @@ const NavBar = ({ id, onSelectOption, selectedTab }) => {
           </li>
         );
       })}
-      {showIllnessHistoryForm && (
-        <>
-          <IllnessHistoryForm
-          isOpen={showIllnessHistoryForm}
-          onClose={handleCloseIllnessHistoryForm}
-          initialValues={{
-            illness_history_patient_id: id,
-          }}
-          onSubmit={submitIllnessForm}
-        />
-        </>
-      )}
-      {isChorobyTab && (
-        <button onClick={handleIllnessHistoryForm} className="create_visit">
-          Przypisz chorobę
-        </button>
-      )}
       {isDokumentacjaTab && <button onClick={handleCreatePrescription} className="create_visit">Utwórz Receptę</button>}
       {showPrescriptionForm && (
         <>
