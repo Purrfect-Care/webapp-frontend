8f9bd47a2335d7d30111842e7636dcd2a0f0dd9b
Hanna Pieniążek <79666838+hannapieniazek@users.noreply.github.com>
2023-12-17 23:27:07 +0100

    add style and popups to show visit subtypes

--- vet-app/src/pages/ShowPage/ShowVisitSubtypes/ShowVisitSubtypesComponent.jsx
+++ vet-app/src/pages/ShowPage/ShowVisitSubtypes/ShowVisitSubtypesComponent.jsx
@@ -4,10 +4,17 @@ import AddBreed from '../../AddPage/AddBreed';
 import { allBreedsRequest, deleteBreedRequest } from '../../../api/breedRequests';
 import AddVisitSubtype from '../../AddPage/AddVisitSubtype';
 import { visitSubtypeRequest, deleteVisitSubtypeRequest } from '../../../api/visitSubtypeRequest';
+import Snackbar from "@mui/material/Snackbar";
+import MuiAlert from "@mui/material/Alert";
+
 const ShowVisitSubtypeComponent = () => {
     const [visit_subtypeData, setVisitSubtypeData] = useState([]);
     const [openEditForm, setOpenEditForm] = useState(false);
     const [selectedVisitSubtype, setSelectedVisitSubtype] = useState([]);
+    const [openTable, setOpenTable] = useState(true);
+    const [snackbarOpen, setSnackbarOpen] = useState(false);
+    const [snackbarSeverity, setSnackbarSeverity] = useState("success");
+    const [snackbarMessage, setSnackbarMessage] = useState("");
 
     useEffect(() => {
         const fetchData = async () => {
@@ -23,6 +30,12 @@ const ShowVisitSubtypeComponent = () => {
         fetchData();
     }, []);
 
+    const openSnackbar = (severity, message) => {
+        setSnackbarSeverity(severity);
+        setSnackbarMessage(message);
+        setSnackbarOpen(true);
+      };
+
     const handleDelete = async (visit_subtypeId) => {
         try {
             // Call the delete function from the provided onDelete prop
@@ -30,14 +43,17 @@ const ShowVisitSubtypeComponent = () => {
             console.log('VisitSubtype deleted successfully');
             const data = await visitSubtypeRequest();
             setVisitSubtypeData(data);
+            openSnackbar("success", "Podtyp wizyty usunięty pomyślnie!");
         } catch (error) {
             console.error('Error deleting visit_subtype:', error);
+            openSnackbar("error", "Błąd podczas usuwania podtypu wizyty.");
         }
     };
 
     const editVisitSubtype = (visit_subtype) => {
         setSelectedVisitSubtype(visit_subtype);
         setOpenEditForm(true);
+        setOpenTable(false);
       };
 
 
@@ -46,6 +62,7 @@ const ShowVisitSubtypeComponent = () => {
         setSelectedVisitSubtype(null);
         const data = await visitSubtypeRequest();
         setVisitSubtypeData(data);
+        setOpenTable(true);
       };
 
 
@@ -56,11 +73,29 @@ const ShowVisitSubtypeComponent = () => {
     { key: 'visit_subtype_name', label: 'Nazwa podtypu wizyty' },]
 
     return (
+        <>
         <div>
-            <DynamicTable columns={columns} data={visit_subtypeData} onDelete={handleDelete} onEdit={editVisitSubtype} title={"Podtypy wizyt"} />
-            {openEditForm && (<AddVisitSubtype initialValues={selectedVisitSubtype} onClose={closeForm}/>)}
+            {openTable && <DynamicTable columns={columns} data={visit_subtypeData} onDelete={handleDelete} onEdit={editVisitSubtype} title={"Podtypy wizyt"} />}
+            {openEditForm && (<AddVisitSubtype initialValues={selectedVisitSubtype} onClose={closeForm} snackbar={openSnackbar}/>)}
 
         </div>
+        <Snackbar
+        open={snackbarOpen}
+        anchorOrigin={{ vertical: "top", horizontal: "right" }}
+        autoHideDuration={6000}
+        onClose={() => setSnackbarOpen(false)}
+      >
+        <MuiAlert
+          elevation={6}
+          variant="filled"
+          onClose={() => setSnackbarOpen(false)}
+          severity={snackbarSeverity}
+        >
+          {snackbarMessage}
+        </MuiAlert>
+      </Snackbar>
+        </>
+
     );
 };
 
